0f1fad41e013977f79abc2d976d488891d432c68
mufeed <mufeedm533@gmail.com>
2018-06-12 17:04:16 +0530

    added basic script 37

---
+++ basics/script_37.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to display your details like name, age, address in three different lines
+"""
+name = "mufeed"
+age = 22
+addres = "xyz"
+print("name:",name)
+print("age:",age)
+print("address:",addres)
