0e023a7811ac193f271c47b8702f40aed63b7d91
mufeed <mufeedm533@gmail.com>
2018-06-12 17:06:41 +0530

    added basic script 37

---
+++ basics/script_38.py
@@ -0,0 +1,11 @@
+"""
+Write a Python program to solve (x + y) * (x + y). Go to the editor
+Test Data : x = 4, y = 3
+Expected Output : (4 + 3) ^ 2) = 49
+"""
+
+x = int(input())
+y = int(input())
+
+# utilise python power function
+print(pow((x+y),2))
