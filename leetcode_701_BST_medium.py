3d8e9e98614e87c57b6c2fee1d0a75713590598e
Muhammed Mufeed k <mufeedm533@gmail.com>
2022-11-05 16:21:19 +0530

    Create leetcode_701_BST_medium.py

---
+++ leetcode_701_BST_medium.py
@@ -0,0 +1,25 @@
+"https://leetcode.com/problems/insert-into-a-binary-search-tree/submissions/837288703/"
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return TreeNode(val)
+
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+
+        return root
+
