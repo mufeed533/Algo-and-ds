fa53015e037609c577dd0c4487dfece8308384ff
Muhammed Mufeed k <mufeedm533@gmail.com>
2022-11-18 06:55:59 +0530

    Create leetcode_235_tree_medium.py

---
+++ tree/leetcode_235_tree_medium.py
@@ -0,0 +1,26 @@
+"https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/"
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        curr = root
+
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                return curr
