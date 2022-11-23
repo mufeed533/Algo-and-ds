076821c4863513978da07a76f7d9501b021ecd50
Konaje <Mufeed.Konaje@commscope.com>
2022-11-23 23:04:47 +0530

    Added initial bit manipulation folder

---
+++ bit_manipulation/leetcode_1919_bits_easy.py
@@ -0,0 +1,16 @@
+"https://leetcode.com/problems/number-of-1-bits/description/"
+
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+
+        while n:
+            res += n % 2
+            n = n >> 1
+
+        return res
