2eee56af10da8defeea7e8f96c07ccce34f189cf
Muhammed Mufeed k <mufeedm533@gmail.com>
2022-11-26 22:08:10 +0530

    Create leetcode_94_dfs_easy.py

---
+++ tree/leetcode_94_dfs_easy.py
@@ -0,0 +1,30 @@
+"https://leetcode.com/problems/binary-tree-inorder-traversal/description/"
+
+#Iterative solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()  # the last element
+            if node:
+                if visited:
+                    res.append(node.val)
+                else:
+                    stack.append((node.right, False))
+                    stack.append((node, True))
+                    stack.append((node.left, False))
+        return res
+
+
+        
