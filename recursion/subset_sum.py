4fe8252fd53875d295f32ca2025e973996759611
Muhammed Mufeed K <MuhammedMK@mymiratech.com>
2023-01-22 15:28:29 +0530

    Added subset sum using recursion method

--- recursion/subset_sum.py
+++ recursion/subset_sum.py
@@ -3,20 +3,20 @@ Write a recursive function to check a subset sum is possible from an array of in
 """
 
 
-# ToDo: Finish the code
 def subset_sum(v: list, n: int, s: int) -> bool:
     if s == 0:
-        if n > 0:
-            return False
         return True
 
-    if v[n - 1] > s:
+    if n == 0:
         return False
+
+    if v[n - 1] > s:
+        return subset_sum(v, n - 1, s)
     else:
         return subset_sum(v, n - 1, s - (v[n - 1])) or subset_sum(v, n - 1, s)
 
 
-print(subset_sum([1, 2, 3, 4], 4, 1))
+print(subset_sum([1, 2, 3, 4], 4, 11))
 """
 
 """
